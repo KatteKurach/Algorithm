2bd3b8e8813194d88bf8aa28ec335d16f6810002
KatteKurach <kattekurach@gmail.com>
2016-05-15 18:33:56 +0300

    Search added.

---
+++ heap.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class Heap()
+{
+    public:
+
+        Heap()
+        {
+            
+        }
+
+        void push()
+        {
+
+        }
+
+        void front()
+        {
+
+        }
+
+        void pop()
+        {
+            
+        }
+
+        void back()
+        {
+
+        }
+
+        bool empty()
+        {
+
+        }
+
+        int size()
+        {
+
+        }
+};
+
+int main()
+{
+
+}
+
+
+
